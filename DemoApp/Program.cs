dce0487e36d0d6dc2d704fbd673bbcea75e49069
笠浪貴大 <dev.kacha@gmail.com>
2024-06-13 21:39:50 +0900

    DemoApp追加

---
+++ DemoApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using Ksnm.Units.Japanese;
+using Ksnm.Units;
+
+{
+    平成<int> h = 36;
+
+    令和<int> r = (令和<int>)h;
+
+    Console.WriteLine($"平成{h}=令和{r}");
+
+}
+{
+    令和<int> r = 6;
+    昭和<int> s = (昭和<int>)r;
+
+    Console.WriteLine($"令和{r}って昭和{s}");
+}
